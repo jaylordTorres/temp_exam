84b568b7a4376f6d3cdfc1df230329328f512b36
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-13 04:26:57 +0800

    separated thumb to assets

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import path from 'path'
 import cors from 'cors'
 import express from 'express'
 import router from './routes'
@@ -7,6 +8,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/assets', express.static(path.join(__dirname, '../assets')))
+/// separated thumb @reason thumb is temp files for now
+app.use('/thumb', express.static(path.join(__dirname, '../thumb')))
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
