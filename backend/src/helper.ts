ea3553b550ba8ab80e3a15017461058ccc714633
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-13 04:35:03 +0800

    fix image url

--- backend/src/helper.ts
+++ backend/src/helper.ts
@@ -12,9 +12,9 @@ export const createLinkAsset = (req: Request, photo?: IPhoto): IImageWithThumbs 
   const id = photo?.id || ''
   const ext = name.split('.').slice(-1)[0];
   return {
-    url: `${host}/assets/${name}`,
-    xs: `${host}/thumb/${id}-xs.${ext}`,
-    sm: `${host}/thumb/${id}-sm.${ext}`,
-    md: `${host}/thumb/${id}-md.${ext}`
+    url: `http://${host}/assets/${name}`,
+    xs: `http://${host}/thumb/${id}-xs.${ext}`,
+    sm: `http://${host}/thumb/${id}-sm.${ext}`,
+    md: `http://${host}/thumb/${id}-md.${ext}`
   }
 }
