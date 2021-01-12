971ed9761024b4efef357129ffafab0750a695ec
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-13 03:43:54 +0800

    setup server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,18 @@
 import app from './app'
+import { iniitalize } from './service/db'
 
 const port = parseInt(process.env.APP_PORT as string) || 3000
 
-app.listen(port, () => {
-  const logYellow = '\x1b[33m%s\x1b[0m';
-  console.log(logYellow, `💾 running on ${port}`)
-})
+async function startApp() {
+  /// initialize db
+  /// added seed data to memory db from json
+  /// create thumbs from provideed images 
+  await iniitalize();
+
+  app.listen(port, () => {
+    const logYellow = '\x1b[33m%s\x1b[0m';
+    console.log(logYellow, `💾 running on ${port}`)
+  })
+
+}
+startApp();
