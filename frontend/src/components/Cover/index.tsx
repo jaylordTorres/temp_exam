a1914d0eda0cc70c320c5b5a07381bb3d5aa5cb0
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-14 01:30:23 +0800

    implement image cacge

--- frontend/src/components/Cover/index.tsx
+++ frontend/src/components/Cover/index.tsx
@@ -1,29 +1,24 @@
-import React, { useMemo } from 'react'
+import React, { memo } from 'react'
 import { Spacing } from '../../styles'
 import { useScreenDimensions } from '../../hooks/useScreenDimensions'
-
-import { Image } from './styles'
+import { Image } from "react-native-expo-image-cache";
 
 interface Props {
   source: string
 }
 
-// Cover has to fill the whole card area.
 const Cover = ({ source }: Props) => {
   const size = useScreenDimensions()
-
-  /// memoize props
-  const imageProps = useMemo(() => ({
-    source: { uri: source },
-    style: {
-      width: size.width - Spacing.padding * 2,
-      height: size.width * 0.67,
-    }
-  }), [size, source]);
-
   return (
-    <Image {...imageProps} />
+    <Image
+      uri={source}
+      style={{
+        width: size.width - Spacing.padding * 2,
+        height: size.width * 0.67,
+        resizeMode: 'cover'
+      }}
+    />
   )
 }
 
-export default Cover
+export default memo(Cover)
