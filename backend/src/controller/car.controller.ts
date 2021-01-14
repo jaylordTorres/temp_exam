0bb225e2cec0e0bf1447936051bcdf3e272c18e8
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-15 03:47:59 +0800

    initial crud api

--- backend/src/controller/car.controller.ts
+++ backend/src/controller/car.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Request, Response, Router, NextFunction } from 'express'
 import { createLinkAsset } from '../helper';
 import { getCarPhoto, getCars } from '../service/car.service';
 import { ICar } from '../type';
@@ -7,8 +7,14 @@ class BaseController {
   url = ''
   static addRouter(router: Router, controller: BaseController) {
     router.get(controller.url, controller.index)
+    router.post(controller.url, controller.create)
+    router.put(controller.url + ':id', controller.update)
+    router.delete(controller.url + ':id', controller.remove)
   }
-  index(_req: Request, res: Response) { }
+  index(req: Request, res: Response, next: NextFunction) { }
+  create(req: Request, res: Response, next: NextFunction) { }
+  update(req: Request, res: Response, next: NextFunction) { }
+  remove(req: Request, res: Response, next: NextFunction) { }
 }
 
 
@@ -42,6 +48,69 @@ export default class CarController extends BaseController {
 
     return res.json({
       data: carWithPhotos
+    })
+  }
+
+  /**
+   * 
+   * @param req [Request]
+   * @param res [Response]
+   * @returns [ICar[]]
+   * 
+   */
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      console.log(req.body)
+    } catch (e) {
+      console.log(e.message)
+      return res.status(400).send({ message: e.message })
+    }
+
+    return res.json({
+      data: req.body
+    })
+  }
+  /**
+   * 
+   * @param req [Request]
+   * @param res [Response]
+   * @returns [ICar[]]
+   * 
+   */
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      console.log(req.body)
+      console.log(req.params.id)
+    } catch (e) {
+      console.log(e.message)
+      return res.status(400).send({ message: e.message })
+    }
+
+    return res.status(400).send('sample error')
+
+    return res.json({
+      data: req.body
+    })
+  }
+  /**
+   * 
+   * @param req [Request]
+   * @param res [Response]
+   * @returns [ICar[]]
+   * 
+   */
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      console.log(req.body)
+      console.log(req.params.id)
+    } catch (e) {
+      console.log(e.message)
+      return res.status(400).send({ message: e.message })
+    }
+
+    return res.status(400).send('sample delte error')
+    return res.json({
+      data: req.body
     })
   }
 }
