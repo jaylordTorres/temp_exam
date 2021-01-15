221ca1f582900d81ed6d07240c7d1fa08ec4d5a9
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-15 17:15:54 +0800

    refactor garage

--- frontend/src/screens/Garage/index.tsx
+++ frontend/src/screens/Garage/index.tsx
@@ -1,65 +1,55 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { FlatList, View, Text } from 'react-native'
-import { StackNavigationProp } from '@react-navigation/stack'
-import { getList } from '../../services/api'
-import { ICar, RootStackParamList } from '../../types'
-import CardListItem from '../../components/CardListItem'
-import { Space, Title } from './styles'
+import React, { useCallback, useMemo } from 'react'
+import { FlatList, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import { FormTypes } from '../../constants'
-
+import { StackNavigationProp } from '@react-navigation/stack'
+import { ICar, RootStackParamList } from '../../types'
+import { CardListItem } from '../../components'
+import { useGarageHook } from './hook'
+import { Space, Title, Actions, AddText, Center, Caption } from './styles'
 
 interface GarageProps {
   navigation: StackNavigationProp<RootStackParamList>
 }
 
 const Garage = ({ navigation }: GarageProps) => {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const updateData = async () => {
-      try {
-        const res = await getList()
-        setData(res.data)
-
-      } catch (e) {
-        console.log(e.message);
-      }
-    }
-    updateData()
-  }, [])
-
-  const onPress = useCallback((car) => {
-    return navigation.navigate('Details', car)
-  }, [])
-  const onCreate = useCallback(() => {
-    return navigation.navigate('Form', { type: FormTypes.create })
-  }, [])
-  const Edit = useCallback(() => {
-    return navigation.navigate('Form', { type: FormTypes.edit })
-  }, [])
-
+  const { onCreate, onPress, cars, meta } = useGarageHook({ navigation });
   const renderItem = useCallback(({ item: car }) => {
     return <CardListItem onPress={onPress} car={car} />
   }, [])
 
-  return (
-    <View>
-      <Title>Garage</Title>
-      <TouchableOpacity onPress={onCreate}>
-        <Text>create</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={Edit}>
-        <Text>EDit</Text>
-      </TouchableOpacity>
-      <FlatList
-        data={data}
+
+  const content = useMemo(() => {
+    if (meta.loading) {
+      return <Center>
+        <Caption>...loading</Caption>
+      </Center>
+    }
+
+    if (cars.length > 1) {
+      return <FlatList
+        data={cars}
         renderItem={renderItem}
         keyExtractor={(car: ICar) => car.id}
         ItemSeparatorComponent={Space}
       />
+    }
+
+    return <Center>
+      <Caption>Empty</Caption>
+    </Center>
+  }, [renderItem, cars, meta])
+
+
+  return (
+    <View>
+      <Actions>
+        <Title>Garage</Title>
+        <TouchableOpacity onPress={onCreate}><AddText>Create</AddText></TouchableOpacity>
+      </Actions>
+      {content}
     </View>
   )
+
 }
 
 
