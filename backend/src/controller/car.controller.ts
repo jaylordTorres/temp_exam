8c37989b9062eb0f7fd8c7a003b14d9aa174d475
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-01-16 00:35:55 +0800

    add create func

--- backend/src/controller/car.controller.ts
+++ backend/src/controller/car.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response, Router, NextFunction } from 'express'
 import { createLinkAsset } from '../helper';
-import { removeCarById, getCarById, getCarPhoto, getCars, updateCarById } from '../service/car.service';
+import { createCar, removeCarById, getCarById, getCarPhoto, getCars, updateCarById } from '../service/car.service';
 import { ICar } from '../type';
 import BaseController from './base.cotroller'
+import { genId, randoImage } from '../service/util'
+import { createPhoto } from '../service/photo.service';
+import { optimize } from '../service/upload';
 
 export default class CarController extends BaseController {
   constructor(router: Router) {
@@ -41,39 +44,41 @@ export default class CarController extends BaseController {
    * 
    * @param req [Request]
    * @param res [Response]
-   * @returns [ICar[]]
+   * @returns [ICar]
    * 
    */
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      console.log(req.body)
+      CarController.validateCar(req.body)
+      const { year, model, make_id } = req.body
+      const carId = genId();
+      const photoId = genId();
+      const tempImage = randoImage()
+      await createCar(carId, make_id, model, year)
+      await createPhoto(photoId, carId, tempImage)
+      await optimize(carId, 'assets/' + tempImage, 'jpg')
+      const data = await getCarById(carId)
+      return res.json({ data })
     } catch (e) {
-      console.log(e.message)
       return res.status(400).send({ message: e.message })
     }
 
-    return res.json({
-      data: req.body
-    })
   }
   /**
    * 
    * @param req [Request]
    * @param res [Response]
-   * @returns [ICar[]]
+   * @returns [ICar]
    * 
    */
   async update(req: Request, res: Response, next: NextFunction) {
     try {
-      /// validation
+      CarController.validateCar(req.body)
       const id = req.params.id
       const { year, model, make_id } = req.body
-      if ([year, model, make_id].filter(Boolean).length !== 3) {
-        throw new Error('All fields are required')
-      }
       await updateCarById(model, make_id, year, id)
-      const responseData = await getCarById(id)
-      return res.json({ data: responseData })
+      const data = await getCarById(id)
+      return res.json({ data })
     } catch (e) {
       return res.status(400).send({ message: e.message })
     }
@@ -98,6 +103,12 @@ export default class CarController extends BaseController {
     // return res.json({
     //   data: req.body
     // })
+  }
+
+  static validateCar({ year, model, make_id }: { year: number, make_id: string, model: string }) {
+    if ([year, model, make_id].filter(Boolean).length !== 3) {
+      throw new Error('All fields are required')
+    }
   }
 }
 
